52f3d3c5dbc13a2b70273f03326187e4211a6ab5
dooishe <tugievzx@mail.ru>
2026-01-22 23:58:09 +0300

    IsPolindrom shorcut

--- src/IsPolindrom/Main.java
+++ src/IsPolindrom/Main.java
@@ -3,12 +3,11 @@ package IsPolindrom;
 public class Main {
   public static void main(String[] args) {
    System.out.println(isPalindrome("  text! ,3223fa;"));
-   System.out.println(isPalindrome("к1 2б,  б21 к!"));
+   System.out.println(isPalindrome("к1 2б,  б2   1 К  !    "));
     
   }
   public static boolean isPalindrome(String text) {
-    String editedString = text.replaceAll("\\p{Punct}", "").replaceAll("\\s+", "");
-    String reverseEditedString = new StringBuilder(editedString).reverse().toString();
-    return editedString.equalsIgnoreCase(reverseEditedString); 
+    String editedString = text.replaceAll("[^A-Za-z0-9]", "");
+    return editedString.equalsIgnoreCase(new StringBuilder(editedString).reverse().toString()); 
 }
 }
